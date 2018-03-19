04b2a01b439252373bc3dff8248865f916aa0814
emilia <emilia@courage.iag.usp.br>
2018-03-19 17:49:15 -0300

    order pefi data

---
+++ R/pefi_order_data.R
@@ -0,0 +1,81 @@
+#--------------------------------------------------------------------
+# 
+#
+#--------------------------------------------------------------------
+rm(list=ls())
+
+library(openair)
+
+
+# dados botânico
+path <- '/data1/DATA/LCB/PEFI'
+a <- list.files(path, pattern='TOA5_50319', full.names=T,recursive=T)
+tt <- lapply(a, function(.file)readr::read_csv(.file, skip=4, na="NAN", col_names=F))
+bot <- do.call(rbind,tt)
+names(bot) <- c("date","PTemp","Pira","batt","Dn","Dir","Dx","Sn","Vh","Sx","Tar",
+                "Tpainel","Ur","Press","Chuva_Acm","Vs","batt_Min")
+ref <- data.frame(date=seq.POSIXt(min(bot$date), max(bot$date),by = '1 min'))
+bot <- merge(ref, bot, by="date", all.x=T)
+
+
+# dados zoologico
+labels <- c("date","PTemp","batt","Dn","Dir","Dx","Sn","Vh","Sx","Tar",
+            "Tpainel","Ur","Press","Chuva_Acm","Vs","batt_Min")
+b1 <- list.files(path, pattern='TOA5_60968', full.names=T,recursive=T)[1:2]
+b1 <- lapply(b1, function(.file)readr::read_csv(.file, skip=4, na="NAN", 
+                                                col_names=labels))
+b1 <- do.call(rbind,b1)
+b1 <- unique(b1)
+ref <- data.frame(date=seq.POSIXt(min(b1$date), max(b1$date),by = '1 min'))
+b1 <- merge(ref, b1, by="date", all.x=T)
+
+labels <- c("date","PTemp","batt","Pira_ref","Pira","Dn","Dir","Dx","Sn","Vh","Sx",
+            "Tar","Tpainel","Ur","Press","Chuva_Acm","Vs","batt_Min")
+b2 <- list.files(path, pattern='TOA5_60968', full.names=T,recursive=T)[-c(1:2)]
+b2 <- lapply(b2, function(.file)readr::read_csv(.file, skip=4, na="NAN", 
+                                                col_names=labels))
+b2 <- do.call(rbind,b2)
+b2 <- unique(b2)
+ref <- data.frame(date=seq.POSIXt(min(b2$date), max(b2$date),by = '1 min'))
+b2 <- merge(ref, b2, by="date", all.x=T)
+zoo <- merge(b1, b2, all=T)
+
+
+# calibração Tar e UR do Zoo
+# Tcorr = 0.9919*TarZoo + 0.0841
+# URcorr = 1.0127*URzoo + 1.7695
+
+zoo$Tar_c = 0.9919 * zoo$Tar + 0.0841
+zoo$Ur_c = 1.0127 * zoo$Ur + 1.7695
+
+# conversão piranômetros:
+zoo$Pira_ref_Wm <- zoo$Pira_ref * 14.771
+zoo$Pira_Wm <- zoo$Pira * 15.151
+
+
+#saida
+# temperaturas, ur, vento do zoo.
+out <- merge(bot[,c(1,11,13)], zoo[,c(1,19,20,5,8,13,14,21)], by="date")
+#str(out)
+names(out)[c(2:7,10)] <- c("Tar_Bot","UR_Bot","Tar_Zoo","UR_Zoo","wd","ws","Ki")
+
+out10 <- timeAverage(out, avg.time = "10 min",vector.ws = TRUE)
+out10$Chuva_Acm <- timeAverage(out, avg.time = "10 min",statistic = "sum")$Chuva_Acm
+write.table(out, paste0(path,"/Dados_pefi.csv"), sep = ",", dec =".", row.names = FALSE, na="-9999.0")
+
+# timePlot(zoo,names(zoo)[21:22], group=T)
+
+#  pdf('pira_zoo.pdf', width = 12)
+#  timePlot(zoo,names(zoo)[21:22], group=T, lty=1, ylab='W/m-2')
+#  timePlot(zoo,names(zoo)[21:22], group=F, lty=1, ylab='W/m-2')
+# scatterPlot(zoo, x='Pira_Wm',y='Pira_ref_Wm', pch=20, linear=T,ci.mod=T)
+#  dev.off()
+#Baterias
+# pdf('baterias_pefi.pdf', width=12)
+# timePlot(bot, names(bot)[c(4,16,17)], y.relation = "free",main="Botânico",
+#          auto.text = FALSE)
+# 
+# timePlot(zoo, names(zoo)[c(3,15,16)], y.relation = "free",main="Zoológico",
+#          auto.text = FALSE)
+# dev.off()
+
